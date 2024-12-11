053c72203aa9b234e45aed7e3a6e0d1dfa754d66
LuigiMDev <luigicumaru@gmail.com>
2024-12-11 11:00:03 -0300

    feat: init validation form on symptoms

---
+++ SymptomsProject/Validations/ListValidation.cs
@@ -0,0 +1,17 @@
+﻿using SymptomsProject.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace SymptomsProject.Validations
+{
+    public class ListValidation :ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is IList<object> list && list.Any())
+            {
+                return ValidationResult.Success;
+            }
+            return new ValidationResult(ErrorMessage ?? "Selecione ao menos um item.");
+        }
+    }
+}
